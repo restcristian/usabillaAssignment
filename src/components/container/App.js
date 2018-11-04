d0e2b85aceeba28a70ff5b37cf41cc45556a125f
1526853 <c.a.restituyonunez@wlv.ac.uk>
2018-11-04 17:39:09 -0400

    Fetching and rendering data

--- src/components/container/App.js
+++ src/components/container/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import Header from '../presentational/Header/Header';
 import Filters from '../presentational/Filters/Filters';
 import TableLayout from '../presentational/TableLayout/TableLayout';
+
+import {fetchItemsLocally} from '../../helpers/api';
+
 import styles from './App.css';
 class App extends Component {
     state = {
         items: [],
+        filteredItems:[],
         filters:{
             filter1:{
                 isSelected:false
@@ -23,15 +27,31 @@ class App extends Component {
             filter5:{
                 isSelected:false
             },
-        }
+        },
+        isDataLoading:false,
+    }
+
+    componentDidMount(){
+        
+        this.setState({isDataLoading:true}, ()=> {
+            fetchItemsLocally().then(response =>{
+                this.setState({items:response, filteredItems:response,isDataLoading:false});
+            });
+        });
     }
     render() {
+        let content = null;
+        if(this.state.isDataLoading){
+            content = <span>Loading...</span>;
+        }else{
+            content =  <TableLayout items = {this.state.filteredItems}/>
+        }
         return (
             <div>
                  <Header title="Dashboard" />
                 <div className = {styles.container}>
                     <Filters filters = {this.state.filters} />
-                    <TableLayout />
+                    {content}
                 </div>
             </div>
 
