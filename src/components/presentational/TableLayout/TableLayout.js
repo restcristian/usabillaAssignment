d14aabda79b81ebf9a54cf5fdff0617a2594a179
1526853 <c.a.restituyonunez@wlv.ac.uk>
2018-11-04 17:39:48 -0400

    Modified table layout to adjust data.

--- src/components/presentational/TableLayout/TableLayout.js
+++ src/components/presentational/TableLayout/TableLayout.js
@@ -1,93 +1,115 @@
 import React, { Component } from 'react';
 import styles from './TableLayout.css';
+import {getDevice, trimText} from '../../../helpers/formatHelper';
 
 class TableLayout extends Component {
     state = {
         isMobile: false
     }
 
-    componentDidMount(){
+    componentDidMount() {
         window.addEventListener('resize', this.checkWindowSize);
         this.checkWindowSize();
     }
 
-    shouldComponentUpdate(nextProps, nextState){
+    shouldComponentUpdate(nextProps, nextState) {
         return this.state.isMobile !== nextState.isMobile;
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener('resize', this.checkWindowSize);
     }
 
     render() {
 
         let layout = null;
+        const { items } = this.props;
+        const maxNumOfCharacters = 150;
 
-        if(this.state.isMobile){
+        if (this.state.isMobile) {
             layout = (
-                <div className = {styles.tableLayoutMobile}>
-                <div className = {styles.tableMobileRow}>
-                    <div className = {styles.tableMobileHeading}>
-                        <div className = {styles.mobileRating}>Rating</div>
-                    </div>
-                    <div className = {[styles.mobileContent,styles.mobileRating].join(' ')}>
-                        <span>2</span>
-                    </div>
-                    {/* comment */}
-                    <div className = {styles.tableMobileHeading}>
-                        <div className = {styles.mobileComment}>Comment</div>
-                    </div>
-                    <div className = {[styles.mobileContent,styles.mobileComment].join(' ')}>
-                        <span>lorem impsom ssldasldlsadlasddldlsl</span>
-                    </div>
-                    {/* browser */}
-                    <div className = {styles.tableMobileHeading}>
-                        <div className = {styles.mobileBrowser}>Browser</div>
-                    </div>
-                    <div className = {[styles.mobileContent,styles.mobileBrowser].join(' ')}>
-                        <span>Chrome</span>
-                    </div>
-                    {/* device */}
-                    <div className = {styles.tableMobileHeading}>
-                        <div className = {styles.mobileDevice}>Device</div>
-                    </div>
-                    <div className = {[styles.mobileContent,styles.mobileDevice].join(' ')}>
-                        <span>iPhone 5</span>
-                    </div>
-                    {/* platform */}
-                    <div className = {styles.tableMobileHeading}>
-                        <div className = {styles.mobilePlatfrm}>Platform</div>
-                    </div>
-                    <div className = {[styles.mobileContent,styles.mobilePlatfrm].join(' ')}>
-                        <span>iOS</span>
-                    </div>
+                <div className={styles.tableLayoutMobile}>
+                    {items.map((item, idx) => {
+                        return (
+                            <div key={item.id} className={styles.tableMobileRow}>
+                                <div className={styles.tableMobileHeading}>
+                                    <div className={styles.mobileRating}>Rating</div>
+                                </div>
+                                <div className={[styles.mobileContent, styles.mobileRating].join(' ')}>
+                                    <span>{item.rating}</span>
+                                </div>
+                                {/* comment */}
+                                <div className={styles.tableMobileHeading}>
+                                    <div className={styles.mobileComment}>Comment</div>
+                                </div>
+                                <div className={[styles.mobileContent, styles.mobileComment].join(' ')}>
+                                    <span>{trimText(item.comment, maxNumOfCharacters )}</span>
+                                </div>
+                                {/* browser */}
+                                <div className={styles.tableMobileHeading}>
+                                    <div className={styles.mobileBrowser}>Browser</div>
+                                </div>
+                                <div className={[styles.mobileContent, styles.mobileBrowser].join(' ')}>
+                                    <span>{item.computed_browser.Browser}</span>
+                                    <span>{item.computed_browser.Version}</span>
+                                </div>
+                                {/* device */}
+                                <div className={styles.tableMobileHeading}>
+                                    <div className={styles.mobileDevice}>Device</div>
+                                </div>
+                                <div className={[styles.mobileContent, styles.mobileDevice].join(' ')}>
+                                    <span>{getDevice(item.browser.userAgent)}</span>
+                                </div>
+                                {/* platform */}
+                                <div className={styles.tableMobileHeading}>
+                                    <div className={styles.mobilePlatfrm}>Platform</div>
+                                </div>
+                                <div className={[styles.mobileContent, styles.mobilePlatfrm].join(' ')}>
+                                    <span>{item.computed_browser.Platform}</span>
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
-            </div>
             );
         }
-        else{
+        else {
             layout = (
                 <div className={styles.tableLayout}>
-                <div className={[styles.tableRow, styles.tableHeading].join(' ')}>
-                    <div className={styles.rating}>Rating</div>
-                    <div className={styles.comment}>Comment</div>
-                    <div className={styles.browser}>Browser</div>
-                    <div className={styles.device}>Device</div>
-                    <div className={styles.platfrm}>Platform</div>
+                    <div className={[styles.tableRow, styles.tableHeading].join(' ')}>
+                        <div className={styles.rating}>Rating</div>
+                        <div className={styles.comment}>Comment</div>
+                        <div className={styles.browser}>Browser</div>
+                        <div className={styles.device}>Device</div>
+                        <div className={styles.platfrm}>Platform</div>
+                    </div>
+                    {items.map((item, idx) => {
+                        return (
+                            <div key={item.id} className={[styles.tableRow, styles.contentRow].join(' ')}>
+                                <div className={styles.rating}><span>{item.rating}</span></div>
+                                <div className={styles.comment}><span>{trimText(item.comment, maxNumOfCharacters)}</span></div>
+                                <div className={styles.browser}>
+                                    <span>{item.computed_browser.Browser}</span>
+                                    <span>{item.computed_browser.Version}</span>
+                                </div>
+                                <div className={styles.device}><span>{getDevice(item.browser.userAgent)}</span></div>
+                                <div className={styles.platfrm}><span>{item.computed_browser.Platform}</span></div>
+                            </div>
+                        );
+                    })}
+
                 </div>
-                <div className={[styles.tableRow, styles.contentRow].join(' ')}>
-                    <div className={styles.rating}><span>2</span></div>
-                    <div className={styles.comment}><span>ssdsldsdlsdlsldlsd</span></div>
-                    <div className={styles.browser}><span>Chrome</span></div>
-                    <div className={styles.device}><span>iPhone 5</span></div>
-                    <div className={styles.platfrm}><span>iOS</span></div>
-                </div>
-            </div>
             );
         }
-        
+
         return layout;
     }
+    
 
-    checkWindowSize = (event) =>{
-        this.setState({isMobile:window.innerWidth <= 560});
+    checkWindowSize = (event) => {
+        this.setState({ isMobile: window.innerWidth <= 560 });
     }
+
 
 }
 
