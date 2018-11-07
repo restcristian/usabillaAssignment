1b212dc5d0f0179fcca3d604a09e99aa5a8f31f9
1526853 <c.a.restituyonunez@wlv.ac.uk>
2018-11-06 20:54:36 -0400

    Added url-loader to include images through webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,50 @@
 var path = require('path');
 
 module.exports = {
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test:/\.js$/,
-                exclude:/node_modules/,
-                use:{
-                    loader:"babel-loader"
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
                 }
             },
             {
-                test:/\.css$/,
-                loader:'style-loader'
+                test: /\.css$/,
+                loader: 'style-loader'
             },
             {
-                test:/\.css$/,
-                loader:'css-loader',
-                query:{
-                    modules:true,
-                    localIdentName:'[name]__[local]___[hash:base64:5]'
+                test: /\.css$/,
+                loader: 'css-loader',
+                query: {
+                    modules: true,
+                    localIdentName: '[name]__[local]___[hash:base64:5]'
                 }
             },
             {
-                test:/\.css$/,
-                loader:'postcss-loader'
+                test: /\.css$/,
+                loader: 'postcss-loader'
+            },
+
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192
+                        }
+                    }
+                ]
             }
-            
+
         ]
     },
-    devServer:{
-        contentBase:path.join(__dirname, 'dist'),
-        compress:true,
-        port:5000,
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 5000,
     }
-    
+
 };
