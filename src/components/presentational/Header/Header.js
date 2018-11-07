50da9141d508d4f674b9f01da3ed361e6a05db31
1526853 <c.a.restituyonunez@wlv.ac.uk>
2018-11-06 20:56:40 -0400

    Added dashboard icon.

--- src/components/presentational/Header/Header.js
+++ src/components/presentational/Header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './Header.css';
-
+import dashboardIcon from '../../../assets/imgs/dashboard.png';
 
 const Header = props => {
     return (
         <header>
             <div className={styles.header}>
-                <h1 className={styles.title}>{props.title}</h1>
+                <div className = {styles.headerWrapper}>
+                    <img src={dashboardIcon} alt="Dashboard Icon" />
+                    <h1 className={styles.title}>{props.title}</h1>
+                </div>
             </div>
         </header>
 
